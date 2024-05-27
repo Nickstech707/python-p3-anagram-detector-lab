9f91de2dca31674e85f972d66603eeba4831264c
Nicholas Muriithi <nickstech707@gmail.com>
2024-05-27 17:19:22 +0300

    Takes in a word and a list of possible anagrams and selects the correct one

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = self.sort_letters(word)
+
+    def sort_letters(self, word):
+        return sorted(word)
+
+    def match(self, word_list):
+        return [word for word in word_list if self.sort_letters(word) == self.word_letters]
+
+print(Anagram("listen").match(['enlists', 'google', 'inlets', 'banana']))
